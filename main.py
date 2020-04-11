2524a6bd537c8d739f40a1379f89bbf8b549f1e5
Alexander Boone <alboone@uw.edu>
2020-04-11 16:53:21 -0700

    updated unixtime route to properly return the Unix time

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+'''
+Microservice that returns the Unix Time (AKA epoch time), which is
+the time that has elapsed since 12:00 AM GMT on January 1, 1970, in seconds.
+'''
+
 import os
-import datetime
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/unixtime/')
 def get_unixtime():
-    unixtime = "Placeholder for Unix Time"
-    return unixtime
+    '''Return unix time in seconds'''
+    unixtime = int(time.time())
+    return str(unixtime)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
